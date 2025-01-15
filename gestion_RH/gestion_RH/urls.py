b4f0e9f11c071c1161df6f16064302bcde2fd11b
Nada chaa <140740775+Nada-webdev@users.noreply.github.com>
2025-01-16 00:37:22 +0100

    fixed  ui

--- gestion_RH/gestion_RH/urls.py
+++ gestion_RH/gestion_RH/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('',include('apps.app.urls')),
     path('',include('apps.authentification.urls')),
     path('rh/', include('apps.conge.urls')),
-    path('rh/tables/', include('apps.contrat.urls')),
+    path('', include('apps.contrat.urls')),
     path('rh/tables/', include('apps.competence.urls')),
     path('rh/tables/', include('apps.candidat.urls')),
     path('rh/tables/', include('apps.objectif.urls')),
