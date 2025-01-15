1af2e85d9219e1a5a584f8c6076ce42fc64663f2
SamiSelx <samifcb14@gmail.com>
2025-01-15 21:29:33 +0100

    register ui

--- gestion_RH/apps/authentification/forms.py
+++ gestion_RH/apps/authentification/forms.py
@@ -4,11 +4,17 @@ from django.contrib.auth.models import User
 from .models import CustomUser
 
 class CreateUserForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    email = forms.EmailField(required=True,widget=forms.TextInput(attrs={'placeholder':'Enter your email'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Enter your password'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Confirm your password'}))
+    
     class Meta:
         model = CustomUser
         fields = ('username', 'email', 'password1', 'password2')
 
+        widgets = {
+            'username': forms.TextInput(attrs={'placeholder': 'Enter your username'}),
+        }
     def save(self, commit=True):
         user = super().save(commit=False)
         user.email = self.cleaned_data['email']
